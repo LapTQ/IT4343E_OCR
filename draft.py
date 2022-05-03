b61e2cef4d59032efdd89fd8d59ce0789331cf3e
ProgrammingForFun <lap.tq4@gmail.com>
2022-05-03 22:22:07 +0700

    msg

--- draft.py
+++ draft.py
@@ -7,7 +7,8 @@ from utils.generals import *
 import matplotlib.pyplot as plt
 import json
 
-get_model((124, 1900, 1), len(CHARACTERS), option=1).summary()
+model = get_model((124, 1900, 1), CHAR_TO_NUM.vocabulary_size(), option=1)
+print(model.summary())
 
 # USE TF.DATA
 train_dataset = get_tf_dataset(
@@ -21,8 +22,7 @@ train_dataset = get_tf_dataset(
     # dilate=2,
     normalize=True
 )
-for batch in train_dataset.take(1):
-    images, labels = batch['image'], batch['label']
+for images, labels in train_dataset.take(1):
     pass
 
 
@@ -57,71 +57,83 @@ for img, label in zip(images, labels):
 plt.savefig('draft.jpg')
 plt.show()
 
-chars = ['a', 'b', 'c']
-ctn = keras.layers.StringLookup(vocabulary=chars, oov_token='')
-ntc = keras.layers.StringLookup(vocabulary=chars, oov_token='', invert=True)
+targets = []
+preds = model.predict(images)
+print(preds.shape)
+preds = decode_batch_predictions(preds)
+for label in labels:
+    label = tf.strings.reduce_join(NUM_TO_CHAR(label)).numpy().decode('utf-8')
+    targets.append(label)
+for t, p in zip(targets, preds):
+    print(f'T: {t}')
+    print(f'P: {p}')
 
-def decode_batch_predictions(pred):
-    input_len = np.ones(pred.shape[0]) * pred.shape[1]
-    # Use greedy search. For complex tasks, you can use beam search
-    results = keras.backend.ctc_decode(pred, input_length=input_len, greedy=True)[0][0]
-    # Iterate over the results and get back the text
-    output_text = []
-    for result in results:
-        result = tf.strings.reduce_join(ntc(result)).numpy().decode("utf-8")
-        output_text.append(result)
-    return output_text
-def CTCLoss(y_true, y_pred):
-    # Compute the training-time loss value
-    batch_len = tf.cast(tf.shape(y_true)[0], dtype="int64")
-    input_length = tf.cast(tf.shape(y_pred)[1], dtype="int64")
-    label_length = tf.cast(tf.shape(y_true)[1], dtype="int64")
 
-    input_length = input_length * tf.ones(shape=(batch_len, 1), dtype="int64")
-    label_length = tf.math.count_nonzero(y_true, axis=-1, keepdims=True, dtype="int64") #label_length * tf.ones(shape=(batch_len, 1), dtype="int64")
-    loss = keras.backend.ctc_batch_cost(y_true, y_pred, input_length, label_length)
-    return loss
-
-def process_label(label, target_length=None, padding_values=0):
-    """
-
-    :param label: string of transcript
-    :return:
-    """
-    label = tf.strings.unicode_split(label, input_encoding='UTF-8')
-    label = ctn(label)
-    if target_length is not None:
-        # pad label with padding_values to a unified length (padded_shapes)
-        label = tf.pad(
-            label,
-            [[0, target_length - len(label)]],
-            'CONSTANT',
-            constant_values=padding_values
-        )
-
-    return label
-
-pred = np.array([
-    [[0.1, 0.5, 0.3, 0.1],
-     [0.2, 0.5, 0.2, 0.2],
-     [0.1, 0.1, 0.1, 0.7],
-     [0.1, 0.2, 0.5, 0.2],
-     [0.1, 0.2, 0.5, 0.2],
-     [0.1, 0.5, 0.3, 0.1],
-     [0.2, 0.5, 0.2, 0.2],
-     [0.1, 0.1, 0.1, 0.7],
-     [0.2, 0.5, 0.2, 0.2]
-     ]
-])
-label = 'abaa'
-label = process_label(label)
-label = np.expand_dims(label, axis=0)
-label = np.array([[1, 2, 1, 1, 0, 0, 0, 0, 0]])
-print(label)
-
-a = decode_batch_predictions(pred)
-loss = CTCLoss(label, pred)
-print(a)
-print(loss)
+# chars = ['a', 'b', 'c']
+# ctn = keras.layers.StringLookup(vocabulary=chars, oov_token='')
+# ntc = keras.layers.StringLookup(vocabulary=chars, oov_token='', invert=True)
+#
+# def decode_batch_predictions(pred):
+#     input_len = np.ones(pred.shape[0]) * pred.shape[1]
+#     # Use greedy search. For complex tasks, you can use beam search
+#     results = keras.backend.ctc_decode(pred, input_length=input_len, greedy=True)[0][0]
+#     # Iterate over the results and get back the text
+#     output_text = []
+#     for result in results:
+#         result = tf.strings.reduce_join(ntc(result)).numpy().decode("utf-8")
+#         output_text.append(result)
+#     return output_text
+# def CTCLoss(y_true, y_pred):
+#     # Compute the training-time loss value
+#     batch_len = tf.cast(tf.shape(y_true)[0], dtype="int64")
+#     input_length = tf.cast(tf.shape(y_pred)[1], dtype="int64")
+#     label_length = tf.cast(tf.shape(y_true)[1], dtype="int64")
+#
+#     input_length = input_length * tf.ones(shape=(batch_len, 1), dtype="int64")
+#     label_length = tf.math.count_nonzero(y_true, axis=-1, keepdims=True, dtype="int64") #label_length * tf.ones(shape=(batch_len, 1), dtype="int64")
+#     loss = keras.backend.ctc_batch_cost(y_true, y_pred, input_length, label_length)
+#     return loss
+#
+# def process_label(label, target_length=None, padding_values=0):
+#     """
+#
+#     :param label: string of transcript
+#     :return:
+#     """
+#     label = tf.strings.unicode_split(label, input_encoding='UTF-8')
+#     label = ctn(label)
+#     if target_length is not None:
+#         # pad label with padding_values to a unified length (padded_shapes)
+#         label = tf.pad(
+#             label,
+#             [[0, target_length - len(label)]],
+#             'CONSTANT',
+#             constant_values=padding_values
+#         )
+#
+#     return label
+#
+# pred = np.array([
+#     [[0.1, 0.5, 0.3, 0.1],
+#      [0.2, 0.5, 0.2, 0.2],
+#      [0.1, 0.1, 0.1, 0.7],
+#      [0.1, 0.2, 0.5, 0.2],
+#      [0.1, 0.2, 0.5, 0.2],
+#      [0.1, 0.5, 0.3, 0.1],
+#      [0.2, 0.5, 0.2, 0.2],
+#      [0.1, 0.1, 0.1, 0.7],
+#      [0.2, 0.5, 0.2, 0.2]
+#      ]
+# ])
+# label = 'abaa'
+# label = process_label(label)
+# label = np.expand_dims(label, axis=0)
+# label = np.array([[1, 2, 1, 1, 0, 0, 0, 0, 0]])
+# print(label)
+#
+# a = decode_batch_predictions(pred)
+# loss = CTCLoss(label, pred)
+# print(a)
+# print(loss)
 
 
--- models/crnn.py
+++ models/crnn.py
@@ -2,31 +2,32 @@ import tensorflow as tf
 from tensorflow import keras
 from utils.losses import *
 
-def get_model(image_shape, vocab_size, option=2):
+def get_model(image_shape, vocab_size, option=1):
     # must be None???
     input = keras.layers.Input(shape=(image_shape[0], None, image_shape[2]), name='image')
 
     if option == 1:
         x = keras.layers.Conv2D(
             filters=32,
-            kernel_size=(11, 41),
+            kernel_size=(41, 11),
             strides=(2, 2),
             padding='same',
-            kernel_initializer='he_normal'
-            # use_bias=False
+            # use_bias=False,
+            name='conv_1'
         )(input)
-        x = keras.layers.BatchNormalization()(x)
-        x = keras.layers.LeakyReLU()(x)
+        x = keras.layers.BatchNormalization(name='bn_1')(x)
+        x = keras.layers.LeakyReLU(name='leaky_relu_1')(x)
+
         x = keras.layers.Conv2D(
             filters=32,
-            kernel_size=(11, 21),
-            strides=(1, 2),
+            kernel_size=(21, 11),
+            strides=(2, 2),#(2, 1)
             padding='same',
-            kernel_initializer='he_normal'
-            # use_bias=False
+            # use_bias=False,
+            name='conv_2'
         )(x)
-        x = keras.layers.BatchNormalization()(x)
-        x = keras.layers.LeakyReLU()(x)
+        x = keras.layers.BatchNormalization(name='bn_2')(x)
+        x = keras.layers.LeakyReLU(name='leaky_relu_2')(x)
 
     elif option == 2:
         x = keras.layers.Conv2D(filters=16, kernel_size=(4, 14), strides=2, padding='same',
@@ -52,15 +53,13 @@ def get_model(image_shape, vocab_size, option=2):
         x = keras.layers.BatchNormalization()(x)
         x = keras.layers.LeakyReLU()(x)
 
-
     x = keras.layers.Permute((2, 1, 3))(x)
     # reshape to feed to RNNs
     x = keras.layers.Reshape((-1, x.shape[-2] * x.shape[-1]))(x)
     rnn_layers = 4
     rnn_units = 128
     for i in range(1, rnn_layers + 1):
-        x = keras.layers.Bidirectional(
-            keras.layers.GRU(
+        recurrent = keras.layers.GRU(
                 units=rnn_units,
                 activation='tanh',
                 recurrent_activation='sigmoid',
@@ -68,7 +67,9 @@ def get_model(image_shape, vocab_size, option=2):
                 return_sequences=True,
                 reset_after=True,
                 name=f"gru_{i}",
-            ),
+        )
+        x = keras.layers.Bidirectional(
+            recurrent,
             name=f"bidirectional_{i}",
             merge_mode='concat'
         )(x)
